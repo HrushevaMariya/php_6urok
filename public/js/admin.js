44794c1a6fe6d9149e13fbef03c422e0717d574b
HrushevaMariya <hrusheva.mariya@mail.ru>
2020-12-28 16:34:52 +0700

    DZ_urok_6

---
+++ public/js/admin.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.js-status').change(function () {
+        var id = $(this).data('id');
+        var status = $(this).val();
+
+        $(this).attr('disabled', true);
+        var pthis = this;
+        $.ajax({
+            url: '/admin/status?id='+id+'&status='+status,
+            success: function () {
+                $(pthis).attr('disabled', false);
+            },
+        });
+    });
+});
+© 2020 GitHub, Inc.
